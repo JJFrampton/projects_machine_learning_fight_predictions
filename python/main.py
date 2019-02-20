730736f3744a134c8e34034d46b13107ada6e1ba
joseph frampton <josephjamesframpton@gmail.com>
2019-02-20 06:15:19 -0700

    updated main, added some functions and one hot encoding
    JJF

--- python/main.py
+++ python/main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 # Fight metrics
+# use the simplest approach you can
+# get something working first then tweak,
+# rework, or rewrite
 
 import numpy as np
 import pandas as pd
@@ -17,3 +20,26 @@ print('stopped in round one : %s' %(stopped_in_round_one))
 
 # print(fighters.head)
 # print(fights.head)
+
+# can use oneHot
+heights = fighters['height'].unique().shape[0]
+weights = fighters['weight'].unique().shape[0]
+countries = fighters['country'].unique().shape[0]
+# might be too much for oneHot, try leave one out
+associations = fighters['association'].unique().shape[0]
+classes = fighters['class'].unique().shape[0]
+locality = fighters['locality'].unique().shape[0]
+
+def one_hot(column):
+    labels = column[1:1].name
+    result = pd.get_dummies(column,prefix=[labels])
+    return result
+
+def column_stats(column):
+    n = column.shape[0]
+    cardinality = column.unique().shape[0]
+    cardinality_percent = cardinality / n
+    print("cardinality : %s" %(cardinality))
+    print("cardinality percent : %s" %(cardinality_percent))
+
+binary_classes = one_hot(fighters['class'])
