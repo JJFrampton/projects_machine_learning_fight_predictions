e8470e6a223b4813a77925e9eef629dc1b2f556d
Joseph Frampton <josephjamesframpton@gmail.com>
2022-10-01 15:33:46 -0600

    update

---
+++ main.m
@@ -0,0 +1,5 @@
+fighters = csvread('/Users/josephframpton/code/projects_machine_learning_fight_predictions/ufc_fighters__sherdog_2_23_2016.csv');
+
+printf("Start\n")
+fighters(1,:)
+printf("End\n")
---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+fighters = pd.read_csv("/Users/josephframpton/code/projects_machine_learning_fight_predictions/ufc_fighters__sherdog_2_23_2016.csv")
+fighters.head()
+fighters.tail()
+
+fighter(1,:)
